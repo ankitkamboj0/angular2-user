045aca99e27d1b7a6694fff1c946d795ec1208ce
Jhakas\Ankit <ankitkamboj0@gmail.com>
2017-07-23 23:34:48 +0530

    changed name postServices to PostServices.

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';    
+import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class postServices{
+export class PostServices{
     constructor(private http : Http){
         console.log('Post service Initiallized');
     }
